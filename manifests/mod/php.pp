5c006d7718ae76c4582b1fc14a30d96017a69a62
Juan Treminio <jtreminio@gmail.com>
2014-01-07 01:29:52 -0600

    Only setup php.conf files if package_ensure is present

--- manifests/mod/php.pp
+++ manifests/mod/php.pp
@@ -12,15 +12,18 @@ class apache::mod::php (
   include apache::mod::dir
   Class['apache::mod::mime'] -> Class['apache::mod::dir'] -> Class['apache::mod::php']
 
-  file { 'php5.conf':
-    ensure  => file,
-    path    => "${apache::mod_dir}/php5.conf",
-    content => template('apache/mod/php5.conf.erb'),
-    require => [
-      Class['apache::mod::prefork'],
-      Exec["mkdir ${apache::mod_dir}"],
-    ],
-    before  => File[$apache::mod_dir],
-    notify  => Service['httpd'],
+  if $package_ensure == 'present' {
+    file { 'php5.conf':
+      ensure  => file,
+      path    => "${apache::mod_dir}/php5.conf",
+      content => template('apache/mod/php5.conf.erb'),
+      require => [
+        Class['apache::mod::prefork'],
+        Exec["mkdir ${apache::mod_dir}"],
+      ],
+      before  => File[$apache::mod_dir],
+      notify  => Service['httpd'],
+    }
   }
+
 }
