cf0911ca5dba26e2f7974f7d2d1ccc3867ba1712
Josef Stanicek <josef@stanicek.cz>
2014-03-23 20:54:58 +0100

    PHP config file fixed broken dependencies.

--- manifests/mod/php.pp
+++ manifests/mod/php.pp
@@ -46,7 +46,6 @@ if ((!defined(Class['apache::mod::prefork'])) and (!defined(Class['apache::mod::
     path    => "${::apache::mod_dir}/php5.conf",
     content => template('apache/mod/php5.conf.erb'),
     require => [
-      Class['::apache::mod::prefork'],
       Exec["mkdir ${::apache::mod_dir}"],
     ],
     before  => File[$::apache::mod_dir],
