b5231cf8568d7d1d517cc0526b941721a506fbe5
Josef Stanicek <josef@stanicek.cz>
2014-03-25 09:24:34 +0100

    Modified configuration for FastCGI for FPM.

--- manifests/mod/fastcgi_fpm.pp
+++ manifests/mod/fastcgi_fpm.pp
@@ -1,24 +1,18 @@
 class apache::mod::fastcgi_fpm {
 
-  # Debian specifies it's fastcgi lib path, but RedHat uses the default value
-  # with no config file
-  $fastcgi_lib_path = $::apache::params::fastcgi_lib_path
-
   ::apache::mod { 'fastcgi': }
 
-  if $fastcgi_lib_path {
-    # Template uses:
-    # - $fastcgi_server
-    # - $fastcgi_socket
-    # - $fastcgi_dir
-    file { 'fastcgi-fpm.conf':
-      ensure  => file,
-      path    => "${::apache::mod_dir}/fastcgi-fpm.conf",
-      content => template('apache/mod/fastcgi-fpm.conf.erb'),
-      require => Exec["mkdir ${::apache::mod_dir}"],
-      before  => File[$::apache::mod_dir],
-      notify  => Service['httpd'],
-    }
+  # Template uses:
+  # - $fastcgi_server
+  # - $fastcgi_socket
+  # - $fastcgi_dir
+  file { 'fastcgi-fpm.conf':
+    ensure  => file,
+    path    => "${::apache::mod_dir}/fastcgi-fpm.conf",
+    content => template('apache/mod/fastcgi-fpm.conf.erb'),
+    require => Exec["mkdir ${::apache::mod_dir}"],
+    before  => File[$::apache::mod_dir],
+    notify  => Service['httpd'],
   }
 
 }
